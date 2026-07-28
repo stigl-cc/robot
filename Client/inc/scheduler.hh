7f9371983286b107c7c6cee72cb2a3dcff3d0b24
Stigl <realstigl@gmail.com>
2026-07-29 01:02:28 +0200

    Add task scheduler to Client

---
+++ Client/inc/scheduler.hh
@@ -0,0 +1,54 @@
+#pragma once
+#include <condition_variable>
+#include <functional>
+#include <chrono>
+#include <thread>
+
+class Task {
+    public:
+    typedef std::function<void()> function_t;
+    typedef std::chrono::milliseconds timeunit_t;
+    typedef std::chrono::time_point<std::chrono::steady_clock, timeunit_t> timepoint_t;
+
+    private:
+    std::function<void()> function_;
+
+    bool shouldRepeat_;
+    timeunit_t interval_;
+    timepoint_t nextExecutionPoint_;
+
+    public:
+    Task(std::function<void()> function, timeunit_t interval, bool shouldRepeat = true);
+
+    void schedule();
+    timepoint_t getNextExecutionPoint();
+    bool checkInvoke();
+    bool operator==(const Task&) const;
+    bool operator!=(const Task&) const;
+};
+
+class TaskScheduler {
+    private:
+    static constexpr std::string_view LOG_TAG = "Scheduler";
+    bool shouldTaskSchedulerRun_;
+    std::mutex mutex;
+    std::condition_variable cv_;
+
+    std::thread thread_;
+    std::vector<Task> tasks_;
+
+    void reset_worker();
+    void worker_thread();
+
+    public:
+    void start(bool blocking);
+
+    void registerTask(const Task& task);
+    void unregisterTask(const Task& task);
+
+    TaskScheduler& operator+=(const Task& task);
+    TaskScheduler& operator-=(const Task& task);
+    bool isWorkerRunning();
+
+    void stop();
+};
---
+++ Client/src/scheduler.cc
@@ -0,0 +1,125 @@
+#include <scheduler.hh>
+#include <logger.hh>
+
+#include <functional>
+#include <chrono>
+#include <thread>
+using namespace std::chrono;
+
+Task::Task(function_t function, milliseconds interval, bool shouldRepeat)
+    : function_(function),
+      shouldRepeat_(shouldRepeat),
+      interval_(interval),
+      nextExecutionPoint_() {
+    nextExecutionPoint_ = time_point_cast<milliseconds>(steady_clock::now()) + interval_;
+}
+
+Task::timepoint_t Task::getNextExecutionPoint() {
+    return nextExecutionPoint_;
+}
+
+bool Task::checkInvoke() {
+    time_point<steady_clock, milliseconds> currentTime = time_point_cast<milliseconds>(steady_clock::now());
+    if(currentTime >= nextExecutionPoint_) {
+        function_();
+        nextExecutionPoint_ = shouldRepeat_
+            ? std::max(currentTime, nextExecutionPoint_) + interval_
+            : timepoint_t::max();
+        
+        return true;
+    }
+    return false;
+}
+
+bool Task::operator==(const Task& other) const {
+    return
+        other.shouldRepeat_ == shouldRepeat_ &&
+        other.function_.template target<void()>() == function_.template target<void()>() &&
+        other.interval_ == interval_;
+}
+
+bool Task::operator!=(const Task& other) const {
+    return !(other == *this);
+}
+
+void TaskScheduler::worker_thread() {
+    std::unique_lock lock(mutex);
+
+    while(shouldTaskSchedulerRun_) {
+        Task::timepoint_t earliest_execution_point = Task::timepoint_t::max();
+
+        for(Task& t : tasks_) {
+            Task::timepoint_t execution_point = t.getNextExecutionPoint();
+            if(execution_point < earliest_execution_point)
+                earliest_execution_point = execution_point;
+
+            t.checkInvoke();
+        }
+
+        cv_.wait_until(lock, earliest_execution_point);
+    }
+
+    lock.unlock();
+    cv_.notify_one();
+}
+
+void TaskScheduler::reset_worker() {
+    cv_.notify_one();
+}
+
+void TaskScheduler::start(bool blocking) {
+    if(isWorkerRunning()) {
+        log_tag(LOG_WARN, "Attempted to double start worker thread!");
+        return;
+    }
+
+    shouldTaskSchedulerRun_ = true;
+
+    thread_ = std::thread(&TaskScheduler::worker_thread, this);
+    if(blocking)
+        thread_.join();
+    else
+        thread_.detach();
+}
+
+void TaskScheduler::registerTask(const Task& task) {
+    tasks_.push_back(task);
+    reset_worker();
+}
+
+TaskScheduler& TaskScheduler::operator+=(const Task& task) {
+    registerTask(task);
+    return *this;
+}
+
+void TaskScheduler::unregisterTask(const Task& task) {
+    tasks_.erase(std::remove_if(tasks_.begin(), tasks_.end(), [&task](const Task& t){return t == task;}));
+    reset_worker();
+}
+
+TaskScheduler& TaskScheduler::operator-=(const Task& task) {
+    unregisterTask(task);
+    return *this;
+}
+
+bool TaskScheduler::isWorkerRunning() {
+    std::lock_guard lock(mutex);
+    return shouldTaskSchedulerRun_;
+}
+
+void TaskScheduler::stop() {
+    if(!isWorkerRunning()) {
+        log_tag(LOG_WARN, "Attempted to stop a non-running worker thread!");
+        return;
+    }
+
+    {
+        std::lock_guard lock(mutex);
+        shouldTaskSchedulerRun_ = false;
+    }
+
+    cv_.notify_one();
+
+    std::unique_lock lock(mutex);
+    cv_.wait(lock);
+}
